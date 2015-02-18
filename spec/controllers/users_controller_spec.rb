09e48e78f489b7f991e082080eebe1d70ab9418a
Bryan Lackey <bryan.lackey@gmail.com>
2015-02-18 11:23:21 -0500

    add post create user_controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,4 +13,36 @@ describe UsersController, type: :controller do
       expect(assigns(:user)).to be_a_new(User)
     end
   end
+
+  describe 'POST #create' do 
+    context 'valid attributes' do 
+      it 'creates new user' do
+        expect{
+          post :create, user: FactoryGirl.attributes_for(:user)
+        }.to change(User, :count).by(1) 
+      end
+
+      it 'redirects to users#show' do 
+        post :create, user: FactoryGirl.attributes_for(:user)
+        expect(response).to redirect_to(user_path(User.last.id))
+      end
+    end
+
+    context 'invalid attributes' do 
+      let(:invalid_attributes) { { name: '' } }
+
+      it 'does not create new user' do 
+        expect{
+          post :create, user: invalid_attributes
+        }.to_not change(User, :count)
+      end
+
+      it 're-renders new' do 
+        post :create, user: invalid_attributes
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  
 end
