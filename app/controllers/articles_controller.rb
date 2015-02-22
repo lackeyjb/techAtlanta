48c97fa0485b13a2227f9d93f6e00ca2704386a0
Bryan Lackey <bryan.lackey@gmail.com>
2015-02-22 11:15:46 -0500

    no delete function added yet

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,5 @@ class ArticlesController < ApplicationController
     @articles = Article.paginate(page: params[:page], per_page: 6).order('created_at DESC')
     @already_saved_articles = current_user.articles.map(&:source) unless current_user.nil?
   end
+
 end
